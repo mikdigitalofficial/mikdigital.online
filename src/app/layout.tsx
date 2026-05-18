b2e13a00a1c560e043cb38e1a44d9cf9d4278064
mik <224880424+mikdigitalofficial@users.noreply.github.com>
2026-05-19 04:05:40 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,7 +20,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
+      <head>
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-274YF4P3S5"
+        />
+
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'G-274YF4P3S5');
+          `}
+        </Script>
+
+      </head>
+
       <body>{children}</body>
+
     </html>
   );
 }
