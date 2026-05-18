337b7e835708f04116aae20383fafef5a621d9a2
mik <224880424+mikdigitalofficial@users.noreply.github.com>
2026-05-19 04:24:16 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,30 @@ export default function RootLayout({
     <html lang="en">
 
       <body>
+        {/* Meta Pixel */}
+
+<Script id="meta-pixel" strategy="afterInteractive">
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;
+    n.push=n;
+    n.loaded=!0;
+    n.version='2.0';
+    n.queue=[];
+    t=b.createElement(e);
+    t.async=!0;
+    t.src=v;
+    s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}
+    (window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1303661508025094');
+    fbq('track', 'PageView');
+  `}
+</Script>
 {/* Google Analytics */}
 
 <Script
