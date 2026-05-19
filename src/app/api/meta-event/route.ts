4379444ae69ee18009b06724fd81e70f1d606215
mik <224880424+mikdigitalofficial@users.noreply.github.com>
2026-05-19 05:31:37 +0530

    Update route.ts

--- src/app/api/meta-event/route.ts
+++ src/app/api/meta-event/route.ts
@@ -2,35 +2,53 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
 
-  const body = await req.json();
+  try {
 
-  await fetch(
-    "https://graph.facebook.com/v19.0/1303661508025094/events?access_token=EAAJzWX6k1VEBRa5mKpnj7wrVqbJSKUSzyZCTFfBLyYZBo6cq72lZAZASGGTBwLyKDoINnZBMHLxDtP2s1at4c8dWmjVD56VZCfjIZAAIfxMsnYNhcJhgdHS7mxiymPbvUNFHn7KqVwywHGZBe4hxUBSxnYKeFhiO5rrrtwmEwalOcvEcwvZCeROFq11f2vbBIGwBtlwZDZD",
-    {
-      method: "POST",
+    const body = await req.json();
 
-      headers: {
-        "Content-Type": "application/json",
-      },
+    const response = await fetch(
+      "https://graph.facebook.com/v19.0/1303661508025094/events?access_token=EAAJzWX6k1VEBRa5mKpnj7wrVqbJSKUSzyZCTFfBLyYZBo6cq72lZAZASGGTBwLyKDoINnZBMHLxDtP2s1at4c8dWmjVD56VZCfjIZAAIfxMsnYNhcJhgdHS7mxiymPbvUNFHn7KqVwywHGZBe4hxUBSxnYKeFhiO5rrrtwmEwalOcvEcwvZCeROFq11f2vbBIGwBtlwZDZD",
+      {
 
-      body: JSON.stringify({
-        data: [
-          {
-            event_name: body.event_name,
+        method: "POST",
 
-            event_time: Math.floor(Date.now() / 1000),
+        headers: {
+          "Content-Type": "application/json",
+        },
 
-            action_source: "website",
+        body: JSON.stringify({
 
-            event_source_url: body.url,
-          },
-        ],
-      }),
-    }
-  );
+          data: [
+            {
+              event_name: body.event_name,
 
-  return NextResponse.json({
-    success: true,
-  });
+              event_time: Math.floor(Date.now() / 1000),
+
+              action_source: "website",
+
+              event_source_url: body.url,
+            },
+          ],
+
+        }),
+
+      }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    return NextResponse.json(data);
+
+  } catch (error) {
+
+    console.log(error);
+
+    return NextResponse.json({
+      error: true,
+    });
+
+  }
 
 }
