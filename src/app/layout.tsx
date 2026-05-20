f1a7828f7962e918cd61002443d74bb8e919c237
mik <224880424+mikdigitalofficial@users.noreply.github.com>
2026-05-21 02:41:34 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-
+      
+<!-- Google Tag Manager -->
+  
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MZJ3FNLG');</script>
+<!-- End Google Tag Manager -->
+  
       <body>
         {/* Meta Pixel */}
 
@@ -63,7 +72,12 @@ export default function RootLayout({
     });
   `}
 </Script>
-
+<!-- Google Tag Manager (noscript) -->
+  
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZJ3FNLG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  
 {children}
 
       </body>
