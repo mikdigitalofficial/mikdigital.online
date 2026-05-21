987a2086d919347886dc529d780b6faeb7707d56
mik <224880424+mikdigitalofficial@users.noreply.github.com>
2026-05-22 03:45:25 +0530

    Update route.ts

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -9,7 +9,7 @@ const scope =
 'ZohoCRM.modules.ALL,ZohoCRM.settings.ALL'
 
 const authUrl =
-`https://accounts.zoho.com/oauth/v2/auth?scope=${scope}&client_id=${clientId}&response_type=code&access_type=offline&redirect_uri=${redirectUri}`
+https://accounts.zoho.in/oauth/v2/auth?scope=${scope}&client_id=${clientId}&response_type=code&access_type=offline&redirect_uri=${redirectUri}`
 
 return NextResponse.redirect(authUrl)
 
