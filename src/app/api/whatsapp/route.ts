dc703d3986606a27c4e9aa13c1da1d555f4cf637
mik <224880424+mikdigitalofficial@users.noreply.github.com>
2026-05-22 19:11:57 +0530

    Create route.ts

---
+++ src/app/api/whatsapp/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+
+const body = await req.json()
+
+const response = await fetch(
+`https://graph.facebook.com/v23.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`,
+{
+method: 'POST',
+headers: {
+Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+'Content-Type': 'application/json'
+},
+body: JSON.stringify({
+messaging_product: 'whatsapp',
+to: body.phone,
+type: 'template',
+template: {
+name: 'hello_world',
+language: {
+code: 'en_US'
+}
+}
+})
+}
+)
+
+const data = await response.json()
+
+return NextResponse.json(data)
+
+}
