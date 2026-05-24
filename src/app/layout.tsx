0b5c4a9c2f2c2c55a3f6202e7a8782bc2fd7042a
mik <224880424+mikdigitalofficial@users.noreply.github.com>
2026-05-24 06:27:16 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,31 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Meta Pixel — production only, prevents preview URL noise in Events Manager */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            if (window.location.hostname === 'mikdigital.online') {
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;
+              n.push=n;
+              n.loaded=!0;
+              n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);
+              t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}
+              (window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1303661508025094');
+              fbq('track', 'PageView');
+            }
+          `}
+        </Script>
+
       </head>
 
       <body>
@@ -45,30 +70,6 @@ export default function RootLayout({
             }}
           />
         </noscript>
-
-        {/* Meta Pixel */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;
-            n.push=n;
-            n.loaded=!0;
-            n.version='2.0';
-            n.queue=[];
-            t=b.createElement(e);
-            t.async=!0;
-            t.src=v;
-            s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}
-            (window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-
-            fbq('init', '1303661508025094');
-            fbq('track', 'PageView');
-          `}
-        </Script>
 
         {children}
 
