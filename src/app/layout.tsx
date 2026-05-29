3c3c3bc9a7e21e9b916689ff27971780488c7c80
Mohamed Ibrahim Khangit config --global user.name mik <224880424+mikdigitalofficial@users.noreply.github.com>
2026-05-30 02:53:45 +0530

    fix: correct GTM implementation in Next.js layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,45 +14,61 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="antialiased">
       <head>
-        <Script
-  src="https://www.googletagmanager.com/gtm.js?id=GTM-MZJ3FNLG"
-  strategy="afterInteractive"
-/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </head>
-      <script
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function() {
-        const params = new URLSearchParams(window.location.search);
-        const fbclid = params.get('fbclid');
-
-        if (fbclid) {
-          const fbc = 'fb.1.' + Date.now() + '.' + fbclid;
-          document.cookie = '_fbc=' + fbc + '; path=/; max-age=7776000';
-        }
-      })();
-    `,
-  }}
-/>
       <body>
 
+        {/* Google Tag Manager (noscript) */}
         <noscript>
-  <iframe
-    src="https://www.googletagmanager.com/ns.html?id=GTM-MZJ3FNLG"
-    height="0"
-    width="0"
-    style={{ display: 'none', visibility: 'hidden' }}
-  />
-</noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZJ3FNLG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
 
         <MouseGlow />
         <SmoothScroll>
           <Nav />
           {children}
         </SmoothScroll>
+
+        {/* Google Tag Manager - correct bootstrap snippet */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MZJ3FNLG');
+            `,
+          }}
+        />
+
+        {/* fbclid capture */}
+        <Script
+          id="fbclid-capture"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                const params = new URLSearchParams(window.location.search);
+                const fbclid = params.get('fbclid');
+                if (fbclid) {
+                  const fbc = 'fb.1.' + Date.now() + '.' + fbclid;
+                  document.cookie = '_fbc=' + fbc + '; path=/; max-age=7776000';
+                }
+              })();
+            `,
+          }}
+        />
+
       </body>
     </html>
   );
